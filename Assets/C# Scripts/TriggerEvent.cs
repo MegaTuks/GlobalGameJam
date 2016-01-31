2b58e02c1728fcf40d6ca6a5015e99ef20d14902
watulion <rdo_cnt@hotmail.com>
2016-01-31 00:34:30 -0600

    camera zoom

---
+++ Assets/C# Scripts/TriggerEvent.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TriggerEvent : MonoBehaviour {
+
+	public string labelText = "";
+	public bool hasCollided = false;
+
+	public CameraMovement camera;
+
+	// Use this for initialization
+	void Start () {
+		camera = FindObjectOfType<CameraMovement> ();
+	}
+
+	void OnTriggerEnter2D (Collider2D other)
+	{
+		if (other.GetComponent<CharacterWalk> () == null)
+			return;
+
+		hasCollided = true;
+		camera.focused = true;
+		labelText = "Interact!";
+	}
+
+	void OnTriggerExit2D (Collider2D other)
+	{
+		hasCollided = false;
+		camera.focused = false;
+	}
+
+}
