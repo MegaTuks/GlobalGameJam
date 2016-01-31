e562deea9cfaae3cd5f41c37774cc44d02ebd5f6
watulion <rdo_cnt@hotmail.com>
2016-01-31 06:05:39 -0600

    enter icon

---
+++ Assets/C# Scripts/EnterIcon.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnterIcon : MonoBehaviour {
+
+	public CharacterWalk player;
+	public CameraMovement camera;
+
+	// Use this for initialization
+	void Start () {
+		camera = FindObjectOfType<CameraMovement> ();
+		player = FindObjectOfType<CharacterWalk> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.position = new Vector3 (-19000,0, 0);
+		if (camera.focused) {
+			transform.position = new Vector3 (player.transform.position.x, player.transform.position.y + 900, 0);
+		}
+	}
+}
