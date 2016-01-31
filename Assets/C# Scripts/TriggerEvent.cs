3f20404f88a130618e7bd5358e243ef8a6f5680a
watulion <rdo_cnt@hotmail.com>
2016-01-31 05:40:08 -0600

    room transition

--- Assets/C# Scripts/TriggerEvent.cs
+++ Assets/C# Scripts/TriggerEvent.cs
@@ -5,12 +5,24 @@ public class TriggerEvent : MonoBehaviour {
 
 	public string labelText = "";
 	public bool hasCollided = false;
+	public string destination;
+
 
 	public CameraMovement camera;
 
 	// Use this for initialization
 	void Start () {
 		camera = FindObjectOfType<CameraMovement> ();
+	}
+
+	void Update(){
+		if (hasCollided) {
+			if (Input.GetKey (KeyCode.Return))
+				{
+			Application.LoadLevelAsync (destination);
+				}
+
+		}
 	}
 
 	void OnTriggerEnter2D (Collider2D other)
